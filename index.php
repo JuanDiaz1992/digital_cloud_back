f6983d627e04a1ae888b8408f795358df738dc03
Juan Díaz <juannavegante2010@gmail.com>
2023-10-22 13:12:47 -0500

    Mejoras de código

--- index.php
+++ index.php
@@ -1,12 +1,19 @@
 <?php
 
 require_once "gestionRestauranteSettings/cors.php";
+$os_info = php_uname('s');
 
 
 //Manejo de errores
 ini_set('display_errors',1);
 ini_set('logs_errors',1);
-ini_set('error_log','F:/xampp/htdocs/gestion_restaurante/Error/php_error_log');
+if(strpos($os_info, 'Windows')!== false){
+    ini_set('error_log','F:xampp/htdocs/gestion_restaurante/Error/php_error_log');
+
+}else if(strpos($os_info, 'Linux')!== false){
+    ini_set('error_log','home/Documentos/htdocs/gestion_restaurante/Error/php_error_log');
+
+}
 
 //Manejo de rutas
 require_once "gestionRestauranteSettings/routes_controller.php";
